e2640d7e880ede0f6e0e05fae2165ac29bdb5167
Евгений Добромильский <domkrat@inbox.ru>
2023-11-13 19:46:35 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 sudo cp -s $PWD/winicothumbnailer '/usr/lib/'
 sudo cp -s $PWD/winico.thumbnailer '/usr/share/thumbnailers/'
+read -n1 -r -p "Maybe, you'll need to reboot your system for correct working of icon thumbnailer..." key
